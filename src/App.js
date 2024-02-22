1bba352d6b5c6e2d0959c10752ac8a7a873308ee
sebswit <sebastian.switakowski@gmail.com>
2024-02-22 20:27:56 +0000

    added HTML with hardcoded data and json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=Kettering&appid=24d6ec7f2c3b68a738a5632381fb6339`
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+    <div className="app">
+     <div className="container"></div>
+      <div className="top">
+          <div className="location">
+          <h1>Kettering</h1>
+          </div>
+          <div className="temp">
+          <p>9°C</p>
+          </div>
+          <div className="description">
+          <p>Cloudy</p>
+          </div>            
+      </div>
+    <div className="bottom">
+    <div className="feels">
+          <p>5°C</p>
+          </div> 
+          <div className="humidity">
+          <p>20%</p>
+          </div> 
+          <div className="wind">
+          <p>5 MPH</p>
+          </div> 
+      
+    </div>  
+
     </div>
   );
 }
