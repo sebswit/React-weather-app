d31779c78af29354e8dd990a46b245c512183f49
sebswit <sebastian.switakowski@gmail.com>
2024-02-22 21:26:42 +0000

    added styling, fixed html structure

--- src/App.js
+++ src/App.js
@@ -6,32 +6,36 @@ function App() {
   return (
    
     <div className="app">
-     <div className="container"></div>
-      <div className="top">
-          <div className="location">
-          <h1>Kettering</h1>
-          </div>
-          <div className="temp">
-          <p>9°C</p>
-          </div>
-          <div className="description">
-          <p>Cloudy</p>
-          </div>            
-      </div>
-    <div className="bottom">
-    <div className="feels">
-          <p>5°C</p>
-          </div> 
-          <div className="humidity">
-          <p>20%</p>
-          </div> 
-          <div className="wind">
-          <p>5 MPH</p>
-          </div> 
-      
-    </div>  
-
-    </div>
+     <div className="container">
+        <div className="top">
+            <div className="location">
+            <p>Kettering</p>
+            </div>
+            <div className="temp">
+            <h1>9°C</h1>
+            </div>
+            <div className="description">
+            <p>Cloudy</p>
+            </div>            
+        </div>
+        <div className="bottom">
+            <div className="feels">
+            <p>5°C</p>
+            <p>Feels like</p>
+            </div> 
+            <div className="humidity">
+            <p>20%</p>
+            <p>Humidity</p>
+            </div> 
+            <div className="wind">
+            <p>5 MPH</p>
+            <p>Wind Speed</p>
+            </div>  
+        </div>     
+       </div>  
+     </div>
+    
+   
   );
 }
 
