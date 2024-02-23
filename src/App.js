704c850eb37e58a8026d46b3f8eed7f6a8e93bce
sebswit <sebastian.switakowski@gmail.com>
2024-02-23 19:26:04 +0000

    added location search, font awesome and api

--- src/App.js
+++ src/App.js
@@ -1,37 +1,59 @@
 import React, {useState} from 'react';
 import axios from 'axios';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faWind, faWater } from '@fortawesome/free-solid-svg-icons';
+
 
 function App() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=Kettering&appid=24d6ec7f2c3b68a738a5632381fb6339`
+  const [data,setData] = useState({});
+  const [location, setLocation] = useState ('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=24d6ec7f2c3b68a738a5632381fb6339`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter'){
+    axios.get(url).then((response) => {
+      setData(response.data)
+      console.log(response.data)
+    })
+    setLocation('')
+  }
+  }
   return (
    
     <div className="app">
+      <div className='search'>
+        <input
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyDown={searchLocation}
+        placeholder='Enter location'
+        type='text'/>
+      </div>
      <div className="container">
         <div className="top">
             <div className="location">
-            <p>Kettering</p>
+            <h2>{data.name}</h2>
             </div>
             <div className="temp">
-            <h1>9°C</h1>
+            {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
             </div>
             <div className="description">
-            <p>Cloudy</p>
+               {data.weather ? <h2>{data.weather[0].main}</h2> : null}             
             </div>            
         </div>
-        <div className="bottom">
-            <div className="feels">
-            <p>5°C</p>
-            <p>Feels like</p>
-            </div> 
+        {data.name !== undefined &&
+        <div className="bottom">             
             <div className="humidity">
-            <p>20%</p>
-            <p>Humidity</p>
+            <p className='bold'>{data.main.humidity}%</p>
+            <FontAwesomeIcon icon={faWater}/>
             </div> 
             <div className="wind">
-            <p>5 MPH</p>
-            <p>Wind Speed</p>
+            <p className='bold'>{data.wind.speed.toFixed()} MPH</p>
+            <FontAwesomeIcon icon={faWind}></FontAwesomeIcon>
             </div>  
-        </div>     
+        </div>  
+}   
        </div>  
      </div>
     
